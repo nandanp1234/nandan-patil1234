60efdf787944574c614ee02927816f1c0c0c8fd6
nandanp1234 <86182840+nandanp1234@users.noreply.github.com>
2021-08-06 12:05:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var ComputerBase; 
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+   //Initialising Engine
+  engine = Engine.create();
+  world = engine.world;
+	
+   //Create Player Base and Computer Base Object
+    class ComputerBase{
+     constructor(x,y, width, height){
+   var options = {
+     isStatic: true
+   };
+    
+     this.body = Bodies.rectangle(x, y, width, height, optins);
+     this.width = width;
+     this.height = height;
+     this.image = loadImage("./assets/base2.png");
+
+     World.add(world, this.body);
+     }
+
+    }
+
+ }
+
+function draw() {
+
+  background(180);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+   //Display Playerbase and computer base 
+
+
+   
+   //display Player and computerplayer
+
+
+}
